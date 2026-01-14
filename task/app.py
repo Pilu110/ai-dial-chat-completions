f4d98234cd451f43cea11d53f5fd544a495c8976
Istvan Vincze <istvan_vincze@epam.com>
2026-01-14 16:08:34 +0100

    tutorial 1, finish dial clients

--- task/app.py
+++ task/app.py
@@ -1,55 +1,51 @@
 import asyncio
 from task.clients.client import DialClient
 from task.clients.custom_client import DialClient as CustomClient
-from task.constants import DEFAULT_SYSTEM_PROMPT, API_KEY
+from task.constants import DEFAULT_SYSTEM_PROMPT
 from task.models.conversation import Conversation
 from task.models.message import Message
 from task.models.role import Role
 
 
-async def start(stream: bool) -> None:
+async def start(stream: bool, custom: bool) -> None:
 
-    #TODO:
-    # 1.1. Create DialClient
-    # (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
-    #  you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
-    #  don't forget to add your API_KEY)
-    client = DialClient(deployment_name="gpt-4", api_key=API_KEY)
+    print(f"Running in streaming:{stream} and custom:{custom} mode...\n")
 
-    # 1.2. Create CustomDialClient
-    # custom_client = CustomClient(deployment_name="custom")
-
-    # 2. Create Conversation object
+    client = CustomClient(deployment_name="gpt-4") if custom else DialClient(deployment_name="gpt-4")
     conversation = Conversation()
 
     # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
     #    messages.
     print("Provide System prompt or press 'enter' to continue.")
-    system_prompt = input(">") or DEFAULT_SYSTEM_PROMPT
+    system_prompt = input("System prompt>").strip() or DEFAULT_SYSTEM_PROMPT
     conversation.add_message(Message(Role.SYSTEM, system_prompt))
+
+    print()
 
     # 4. Use infinite cycle (while True) and get yser message from console
 
+    print("Type your question or 'exit' to quit.")
     while True:
         message = input(">")
         # 5. If user message is `exit` then stop the loop
         if message == "exit":
             print("Exiting the chat. Goodbye!")
             break
+
         # 6. Add user message to conversation history (role 'user')
         conversation.add_message(Message(Role.USER, message))
         # 7. If `stream` param is true -> call DialClient#stream_completion()
         if stream:
-            await client.stream_completion(conversation.get_messages())
+            response = await client.stream_completion(conversation.get_messages())
         else:
-            client.get_completion(conversation.get_messages())
-        #    else -> call DialClient#get_completion()
-        # 8. Add generated message to history
+            response = client.get_completion(conversation.get_messages())
+
+        conversation.add_message(response)
+
         # 9. Test it with DialClient and CustomDialClient
         # 10. In CustomDialClient add print of whole request and response to see what you send and what you get in response
-        raise NotImplementedError
 
 
 asyncio.run(
-    start(True)
+    start(True, False)
 )
--- task/clients/client.py
+++ task/clients/client.py
@@ -7,14 +7,14 @@ from task.models.role import Role
 
 class DialClient(BaseClient):
 
-    def __init__(self, deployment_name: str, api_key: str) -> None:
+    def __init__(self, deployment_name: str) -> None:
         super().__init__(deployment_name)
         self._client = Dial(
-            base_url=DIAL_ENDPOINT, api_key=api_key
+            base_url=DIAL_ENDPOINT, api_key=self._api_key
         )
 
         self._async_client = AsyncDial(
-            base_url=DIAL_ENDPOINT, api_key=api_key
+            base_url=DIAL_ENDPOINT, api_key=self._api_key
         )
 
     def get_completion(self, messages: list[Message]) -> Message:
@@ -34,8 +34,6 @@ class DialClient(BaseClient):
 
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # 1. Create chat completions with async client
         chunks = await self._async_client.chat.completions.create(
             deployment_name=self._deployment_name,
         stream=True,
@@ -46,7 +44,7 @@ class DialClient(BaseClient):
             if chunk.choices and len(chunk.choices) > 0:
                 delta = chunk.choices[0].delta
                 if delta and delta.content:
-                    print(delta.content)
+                    print(delta.content, end='')
                     contents.append(delta.content)
 
         print()
